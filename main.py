9a243d83d1060bf1c1045be96c2d3af3351f6f81
skileddeveloperbhardwaj <skilled.developer.bhardwaj@gmail.com>
2024-11-19 09:54:19 +0530

    check

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ def main(page: ft.Page):
         page.update()
 
     def plus_click(e):
-        mydataset = {
-           'cars': ["BMW", "Volvo", "Ford"],
-            'passings': [3, 7, 2]
-        }
+        # mydataset = {
+        #    'cars': ["BMW", "Volvo", "Ford"],
+        #     'passings': [3, 7, 2]
+        # }
 
-        myvar = pd.DataFrame(mydataset)
-        print(type(str(myvar)))
-        # txt_number.value = str(int(txt_number.value) + 1)
-        txt_number.value = str(myvar)
+        # myvar = pd.DataFrame(mydataset)
+        # print(type(str(myvar)))
+        txt_number.value = str(int(txt_number.value) + 1)
+        # txt_number.value = str(myvar)
         page.update()
 
     page.add(
