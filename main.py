a9b315db8a6aa0a4e226ff231ff819691e9a2fc2
skileddeveloperbhardwaj <skilled.developer.bhardwaj@gmail.com>
2024-11-19 09:39:11 +0530

    pandas commit

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ import flet as ft
 import pandas as pd
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Employee Voting Details"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    txt_number = ft.TextField(value="Employee Number", text_align=ft.TextAlign.RIGHT, width=200)
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
         page.update()
 
     def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
+        mydataset = {
+           'cars': ["BMW", "Volvo", "Ford"],
+            'passings': [3, 7, 2]
+        }
+
+        myvar = pd.DataFrame(mydataset)
+        print(type(str(myvar)))
+        # txt_number.value = str(int(txt_number.value) + 1)
+        txt_number.value = str(myvar)
         page.update()
 
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                # ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
                 txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                # ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ft.FilledButton(text="Filled button", on_click=plus_click),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
